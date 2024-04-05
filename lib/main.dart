7d04a1820335f751d89986a2399b45559bb75a65
Shreyash Jagtap <shreyashjagtap.eevv@gmail.com>
2024-04-05 16:48:05 +0530

    Center, Padding, Margin, Box

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_app/widgets/container_sized.dart';
+import "package:flutter/material.dart";
+import "package:flutter_app/widgets/container_sized.dart";
 
 void main() => runApp(new MyApp());
 
--- lib/widgets/container_sized.dart
+++ lib/widgets/container_sized.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class Container_Sized extends StatelessWidget {
   const Container_Sized({super.key});
@@ -7,19 +7,25 @@ class Container_Sized extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Container and Sizedbox'),
-          backgroundColor: Colors.deepOrange, // Set the background color here
+          title: Text('Container And Sized'),
+          backgroundColor: Colors.deepPurple,
         ),
         body: Center(
-            child: Container(
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-            color: Colors.blue,
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+                color: Colors.blue,
+                // borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20)),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 20, spreadRadius: 5, color: Colors.yellow)
+                ]),
+            child: const Center(
+              child: Text('HELLO', style: TextStyle(fontSize: 20)),
+            ),
           ),
-          child: Text('Hello World', style: TextStyle(fontSize: 20)),
-        )
-            // SizedBox(height: 50, width: 50, child: Text("Hello")),
-            ));
+        ));
   }
 }
