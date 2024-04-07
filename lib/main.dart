de27c0c62656b35725545f4ce1f2d6c346d2dec6
Shreyash Jagtap <shreyash5522@gmail.com>
2024-04-08 00:14:34 +0530

    body, text, icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import "package:flutter/material.dart";
-import "package:flutter_app/widgets/container_sized.dart";
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,10 +12,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          brightness: Brightness.dark, primaryColor: Colors.deepPurple),
-      home: Container_Sized(),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: const Text(
+              "Hello World",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: Colors.amberAccent,
+          ),
+          body: Center(
+            child: Container(
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.purpleAccent,
+                  borderRadius: BorderRadius.circular(30)),
+              padding: const EdgeInsets.all(30),
+              child: const Text(
+                "Hello World",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              // child: const Icon(
+              //   Icons.favorite,
+              //   color: Colors.white,
+              //   size: 40,
+              // ),
+            ),
+          ),
+        ));
   }
 }
