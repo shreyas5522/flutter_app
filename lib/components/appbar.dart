0cc5bbc3db7f665f224410bcb2568f3a15300d7b
Shreyash Jagtap <shreyashjagtap.eevv@gmail.com>
2024-04-08 13:40:08 +0530

    My Bookings UI

---
+++ lib/components/appbar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+AppBar buildMyAppBar() {
+  return AppBar(
+    backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+    leading: Icon(
+      Icons.timelapse,
+      size: 25,
+    ),
+    title: const Text("My Bookings",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 25,
+        )),
+    actions: const [
+      Padding(
+        padding: EdgeInsets.all(8),
+        child: Icon(Icons.logout, size: 25, color: Colors.black),
+      ),
+    ],
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,45 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_app/components/appbar.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+List<String> listt = ['All', 'Active', 'Completed', 'Cancelled'];
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-        ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
+        appBar: buildMyAppBar(),
+        body: Column(
+          children: [
+            SizedBox(height: 8), // Add some space between AppBar and the list
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(8),
+                height:
+                    50, // Set the height of the container according to your needs
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: listt.length,
+                  itemBuilder: (context, index) => Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Add your action here
+                      },
+                      child: Text(listt[index]),
                     ),
                   ),
                 ),
               ),
-              //2nd box
-              Expanded(
-                child: Container(
-                  color: Colors.purpleAccent,
-                ),
-              ),
-              //3rd box
-              Expanded(
-                child: Container(
-                  color: Colors.pinkAccent,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
