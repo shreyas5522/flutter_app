f9f956ffb84ec0372f94a106bd8d78a1b94cbdbd
Shreyash Jagtap <shreyashjagtap.eevv@gmail.com>
2024-04-08 10:59:21 +0530

    Coloumns with expanded

--- lib/main.dart
+++ lib/main.dart
@@ -12,47 +12,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            backgroundColor: Colors.deepPurple,
-            title: const Text(
-              "This Title",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2.0,
+                    ),
+                  ),
+                ),
               ),
-            ),
-            centerTitle: true,
-            leading: const Icon(
-              Icons.menu,
-              weight: 700,
-              color: Colors.white,
-            ),
-          ),
-          body: Center(
-            child: Container(
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                  color: Colors.deepPurple,
-                  borderRadius: BorderRadius.circular(30)),
-              padding: const EdgeInsets.all(30),
-              child: const Text(
-                "Hello World",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold),
+              //2nd box
+              Expanded(
+                child: Container(
+                  color: Colors.purpleAccent,
+                ),
               ),
-              // child: const Icon(
-              //   Icons.favorite,
-              //   color: Colors.white,
-              //   size: 40,
-              // ),
-            ),
+              //3rd box
+              Expanded(
+                child: Container(
+                  color: Colors.pinkAccent,
+                ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
