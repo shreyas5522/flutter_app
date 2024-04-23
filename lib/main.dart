cc3e098ba0d460120cc03771cc53877ddb1f39b4
Shreyash Jagtap <shreyashjagtap.eevv@gmail.com>
2024-04-23 12:36:14 +0530

    Click Here: Fucntion Print in Debug Pannel

--- lib/main.dart
+++ lib/main.dart
@@ -9,38 +9,36 @@ List<String> listt = ['All', 'Active', 'Completed', 'Cancelled'];
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key});
+  void userClicked() {
+    print("User Clicked");
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: buildMyAppBar(),
-          body: Stack(
-            alignment: Alignment.center,
-            children: [
-              //big box
-              Container(
-                height: 300,
-                width: 300,
-                color: Colors.amber,
-              ),
-
-              // medium
-              Container(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: buildMyAppBar(),
+            body: Center(
+                child: GestureDetector(
+              onTap: userClicked,
+              child: Container(
                 height: 200,
                 width: 200,
-                color: Colors.orangeAccent[200],
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFFA405B9),
+                ),
+                child: const Center(
+                  child: Text(
+                    "Click Here",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white),
+                  ),
+                ),
               ),
-              // small
-
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.amberAccent,
-              ),
-            ],
-          )),
-    );
+            ))));
   }
 }
