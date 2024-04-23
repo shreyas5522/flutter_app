0ad391c6cfc502192e3c682e2226ae5404cab37f
Shreyash Jagtap <shreyashjagtap.eevv@gmail.com>
2024-04-23 16:41:02 +0530

    App 3

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         appBar: buildMyAppBar(),
         bottomNavigationBar: BottomNavigationBar(
           items: [
-            BottomNavigationBarItem(
+            const BottomNavigationBarItem(
               icon: Icon(
                 Icons.home,
                 size: 30,
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
                   color: Colors.greenAccent,
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: Row(
+                child: const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Icon(
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
               ),
               label: '', // Hide label (optional)
             ),
-            BottomNavigationBarItem(
+            const BottomNavigationBarItem(
               icon: Icon(
                 Icons.person,
                 size: 30,
@@ -63,17 +63,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            SizedBox(height: 8), // Add some space between AppBar and the list
+            const SizedBox(
+                height: 8), // Add some space between AppBar and the list
             Center(
               child: Container(
-                padding: EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
                 height:
                     50, // Set the height of the container according to your needs
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemCount: listt.length,
                   itemBuilder: (context, index) => Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
                     child: ElevatedButton(
                       onPressed: () {},
                       child: Text(listt[index]),
